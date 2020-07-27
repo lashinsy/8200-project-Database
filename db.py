7d6c70b1de62e9f2ff38e073fef02e0803d34090
lashinsy <lch@sem-wolf.org>
2020-07-27 22:47:59 +0300

    add init of class DBTable

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+import shelve
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 from typing import Any, Dict, List
@@ -8,17 +9,45 @@ import os
 @dataclass_json
 @dataclass
 class DBTable(db_api.DBTable):
-    # def __init__(self):
 
+    def __init__(self, name, fields, key_field_name):
+        self.name = name
+        self.fields = fields
+        self.key_field_name = key_field_name
+        with shelve.open(os.path.join(db_api.DB_ROOT, self.name), writeback=True) as db:
+            index = 0
+            for field in self.fields:
+                if field.name != key_field_name:
+                    db[field.name] = [index, field.type]
+                    index += 1
+            self.count_fields = len(db)
 
     def count(self) -> int:
-        raise NotImplementedError
+        with shelve.open(os.path.join(db_api.DB_ROOT, self.name), 'r') as db:
+            count = len(db) - self.count_fields
+        return count
 
-    def insert_record(self, values: Dict[str, Any]) -> None:
-        raise NotImplementedError
+    def insert_record(self, values) -> None:
+        # check if get value for primary key
+        if not values.get(self.key_field_name):
+            raise ValueError("ppp---")
+        with shelve.open(os.path.join(db_api.DB_ROOT, self.name), writeback=True) as db:
+            # check if value of primary key exists in the data
+            if str(values[self.key_field_name]) in list(db.keys()):
+                raise ValueError("ooo---")
+            db[str(values[self.key_field_name])] = [None] * self.count_fields
+            for key in values.keys():
+                # if every key and value that I get is suitable to the data
+                if key != self.key_field_name:
+                    if not db.get(key) or not isinstance(values[key], db[key][1]):
+                        self.delete_record(values[self.key_field_name])
+                        raise ValueError("llll----")
+                    db[str(values[self.key_field_name])].insert(db[key][0], values[key])
 
     def delete_record(self, key: Any) -> None:
-        raise NotImplementedError
+        with shelve.open(os.path.join(db_api.DB_ROOT, self.name), writeback=True) as db:
+            if key in db.keys():
+                del db[key]
 
     def delete_records(self, criteria: List[db_api.SelectionCriteria]) -> None:
         raise NotImplementedError
@@ -39,8 +68,10 @@ class DBTable(db_api.DBTable):
 @dataclass_json
 @dataclass
 class DataBase(db_api.DataBase):
-    tables: Dict[str, DBTable]
+    def __init__(self):
+        self.tables = dict()
 
+    # להוסיף בדיקות האם הטבלה קיימת בכלל
     def create_table(self, table_name, fields, key_field_name) -> DBTable:
         self.tables[table_name] = DBTable(table_name, fields, key_field_name)
         return self.tables[table_name]
@@ -51,9 +82,9 @@ class DataBase(db_api.DataBase):
     def get_table(self, table_name) -> DBTable:
         return self.tables.get(table_name)
 
-# check if shelve create more than one file
+    # check if shelve create more than one file
     def delete_table(self, table_name) -> None:
-        os.remove(db_api.DB_ROOT.joinpath(f"{table_name}.dir"))
+        os.remove(db_api.DB_ROOT.joinpath(f"{table_name}"))
         self.tables.pop(table_name)
 
     def get_tables_names(self) -> List[Any]:
@@ -61,4 +92,3 @@ class DataBase(db_api.DataBase):
 
     def query_multiple_tables(self, tables, fields_and_values_list, fields_to_join_by) -> List[Dict[str, Any]]:
         pass
-
